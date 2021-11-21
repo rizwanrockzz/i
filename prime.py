46d1411c22acb69967001fe8a1dc69f2bb7b02e4
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-11-21 13:23:35 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,14 @@
+def prime(num):
+    flag = True
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = False
+            break
+    return flag
+    
+for i in range(2,100):
+    if prime(i):
+        print(i)
+        
+    else:
+        continue
